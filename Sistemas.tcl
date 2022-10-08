7865a12560150c6a0dfd41522816bcf180893fd6
William Kraus <88288206+KrLiam@users.noreply.github.com>
2022-10-08 11:34:07 -0300

    update project config

--- Sistemas.tcl
+++ Sistemas.tcl
@@ -15,7 +15,7 @@
 
 # Quartus Prime: Generate Tcl File for Project
 # File: Sistemas.tcl
-# Generated on: Thu Oct 06 18:50:42 2022
+# Generated on: Sat Oct 08 11:33:23 2022
 
 # Load Quartus Prime Tcl Project package
 package require ::quartus::project
@@ -59,6 +59,10 @@ if {$make_assignments} {
 	set_global_assignment -name PARTITION_NETLIST_TYPE SOURCE -section_id Top
 	set_global_assignment -name PARTITION_FITTER_PRESERVATION_LEVEL PLACEMENT_AND_ROUTING -section_id Top
 	set_global_assignment -name PARTITION_COLOR 16764057 -section_id Top
+	set_global_assignment -name VHDL_FILE src/ULA.vhd
+	set_global_assignment -name VHDL_FILE src/Sistemas.vhd
+	set_global_assignment -name VHDL_FILE src/RegisterN.vhd
+	set_global_assignment -name VHDL_FILE src/AdderSubtractor.vhd
 	set_global_assignment -name VHDL_FILE src/BinaryCounter.vhd
 	set_global_assignment -name VHDL_FILE src/GenericAdder.vhd
 	set_global_assignment -name VHDL_FILE src/FullAdder.vhd
